0220e5cd19deeb4b28743df5904e7cd05504bf31
Hack Reactor Student <student@hackreactor.com>
2017-10-25 16:03:43 -0700

    Display unique videos on the right and an embedded video in the VideoPlayer component.
    The list on the right is of a dynamic size.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,12 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+  const result = [];
+
+  for (let i = 0; i < props.videos.length; i++) {
+    result[i] = <VideoListEntry video={props.videos[i]} key={i} />; //assign key prop to prevent warning of having the same key 
+  }
+
+  return (<div className="video-list">{result}</div>);  
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
