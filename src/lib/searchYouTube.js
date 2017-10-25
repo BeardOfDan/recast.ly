40c06431747904febab6879cc6cdf04e0a61b0bf
Hack Reactor Student <student@hackreactor.com>
2017-10-25 16:47:33 -0700

    Changed App to  Class

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'url goes here',
+    success: function(data) {
+      // update the state
+      
+    },
+    error: function(data) {
+      // tell the user
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
